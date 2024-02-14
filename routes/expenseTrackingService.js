d5eab4f354f01766a5cf405dedc5173544263142
The-TUSK <marrubio07@gmail.com>
2024-02-14 10:00:06 -0800

    adding create new expense functionality

--- routes/expenseTrackingService.js
+++ routes/expenseTrackingService.js
@@ -37,4 +37,47 @@ router.get("", async (req, res) => {
     }
   });
 
+  router.post("", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
   
+    try {
+      if (token) {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) {
+          return res.status(401).send("Access Denied / Unauthorized request");
+        }
+  
+        const newExpense = new Expenses({
+          amount: req.body.amount,
+          date: req.body.date,
+          category: req.body.category,
+          description: req.body.description,
+          userId: verified.userId,
+          groupId: req.body.groupId,
+        });
+  
+        await newExpense.save();
+        res.status(201).json({
+          message: "Expense created successfully",
+          data: newBudget,
+        });
+      } else {
+        const newExpense = new Expenses({
+            amount: req.body.amount,
+            date: req.body.date,
+            category: req.body.category,
+            description: req.body.description,
+            userId: null,
+            groupId: req.body.groupId,
+          });
+    
+          await newExpense.save();
+        res.status(201).json({
+          message: "Expense created successfully",
+          data: newBudget,
+        });
+      }
+    } catch (error) {
+      res.status(500).send(error.toString());
+    }
+  });
