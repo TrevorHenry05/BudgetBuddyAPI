238410cd2a4d87d07d92beb514311c6fcbdf1e84
TrevorHenry05 <trevorjhen03@gmail.com>
2024-02-23 13:43:27 -0800

    Added logic to format dates with out time stamps when saved.

--- models/expense.js
+++ models/expense.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
+// Function to format date to YYYY-MM-DD
+function formatDateToString(value) {
+  const date = new Date(value);
+  return date.toISOString().split('T')[0];
+}
+
 const ExpenseSchema = new mongoose.Schema(
   {
     amount: Number,
-    date: Date,
+    date: {
+      type: String,
+      set: formatDateToString
+    },
     description: String,
     budgetId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -14,20 +23,30 @@ const ExpenseSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-    }, // Nullable
+    },
     groupId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Group",
       default: null,
-    }, // Nullable
+    },
     categoryId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "ExpenseCategory",
     },
   },
-  { timestamps: true }
+  { 
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
 );
+
+// Virtual for `date` as a Date object
+ExpenseSchema.virtual('dateAsDate').get(function() {
+  return this.date ? new Date(this.date) : null;
+});
 
 const Expense = mongoose.model("Expense", ExpenseSchema);
 
 module.exports = Expense;
+
