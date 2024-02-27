bec26bfa6c1a08cdd24bb4b0d6ef7827df9a8f64
TrevorHenry05 <trevorjhen03@gmail.com>
2024-02-27 14:20:01 -0800

    Updated path

--- routes/expenseCategoryService.js
+++ routes/expenseCategoryService.js
@@ -4,7 +4,7 @@ const ExpenseCategory = require("../models/expenseCategory");
 const router = express.Router();
 
 // Get all expense categories
-router.get("/", async (req, res, next) => {
+router.get("/all", async (req, res, next) => {
   try {
     const expenseCategories = await ExpenseCategory.find({}).toArray();
     res.status(200).json(expenseCategories);
